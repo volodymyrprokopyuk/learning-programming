6de09c70edf4477f5c5008596cf95a0553fecadf
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-05-19 18:08:27 +0200

    feat(bash): add ptree with ps and jq

---
+++ bash-basics/ptree.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Parse process information and create JSON for each process
+ps -ef | head -n 6 | grep -v '^UID' | sort -n -k 2 | while read process; do
+    read user pid ppid c stime tty time cmd <<<$process
+    echo "$user|$pid|$cmd"
+    # jq -nR --arg user "$user" --arg pid "$pid" --arg cmd "$cmd" '{user: $user, pid: $pid, cmd: $cmd}'
+done | jq -nR '(inputs | split("|")) as $process | {user: $process[0], pid: $process[1], cmd: $process[2]}'
