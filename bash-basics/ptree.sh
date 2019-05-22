6e4f7ac847211ca742bfdeba01a7f91868d30ee7
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2019-05-22 10:10:02 +0200

    feat(bash): add PLIMIT to ptree.sh

--- bash-basics/ptree.sh
+++ bash-basics/ptree.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+PLIMIT=6
+
 # Parse process information and create JSON for each process
-ps -ef | head -n 6 | grep -v '^UID' | sort -n -k 2 | while read process; do
+ps -ef | head -n $PLIMIT | grep -v '^UID' | sort -n -k 2 | while read process; do
     read user pid ppid c stime tty time cmd <<<$process
     echo "$user|$pid|$cmd"
     # jq -nR --arg user "$user" --arg pid "$pid" --arg cmd "$cmd" '{user: $user, pid: $pid, cmd: $cmd}'
