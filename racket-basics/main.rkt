4d1bf1650794d96b797141c1020f73e17bd54349
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-04-22 08:37:58 +0200

    feat(racket): add guess numbers game

--- racket-basics/main.rkt
+++ racket-basics/main.rkt
@@ -1,3 +1,21 @@
-#lang racket/base
+;; #lang racket/base
 
-(print "Hello Vlad and Lana")
+(define lower 0)
+
+(define upper 100)
+
+(define (start m n)
+  (set! lower (min m n))
+  (set! upper (max m n))
+  (guess))
+
+(define (guess)
+  (quotient (+ lower upper) 2))
+
+(define (smaller)
+  (set! upper (max lower (sub1 (guess))))
+  (guess))
+
+(define (bigger)
+  (set! lower (min upper (add1 (guess))))
+  (guess))
