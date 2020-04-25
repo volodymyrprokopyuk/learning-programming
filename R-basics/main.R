657e4504381c1ad95f0ca48c17d247ba15e08a65
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-04-25 08:47:20 +0200

    feat(R): add closure rexamples

--- R-basics/main.R
+++ R-basics/main.R
@@ -6,6 +6,33 @@ f1 <- function(x) {
     }
 }
 
-f1(1)()
+# f1(1)()
 
 # 1 + (2 * 3) equivalent `+`(1, `*`(2, 3))
+
+# Name masking
+f2 <- function() {
+    x <- 1
+    y <- 2
+    c(x, y)
+}
+
+# f2()
+rm(f2)
+
+x <- 10
+f3 <- function() {
+    y <- 20
+    c(x, y)
+}
+# f3()
+rm(x, f3)
+
+# Closures
+f4 <- function(x) {
+    y <- 2
+    function() {
+        c(x, y)
+    }
+}
+f4(1)()
