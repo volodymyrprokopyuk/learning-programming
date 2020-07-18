58cfa19e0194cb7077d7283709e03923f6484725
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-07-18 09:11:16 +0200

    feat(scheme): add vector library examples

--- scheme-basics/bin/run.sh
+++ scheme-basics/bin/run.sh
@@ -2,12 +2,14 @@
 
 set -eu
 
-ROOT_DIR=$(pwd)
+readonly ROOT_DIR=$(pwd)
+readonly SRFI_DIR=$HOME/Projects/scheme-upgrade
+
+export GUILE_LOAD_PATH="${ROOT_DIR}:${SRFI_DIR}"
 
 # Chapters 1-5
-# guile3.0 -L "${ROOT_DIR}" a1-tspl-2009.scm
+# guile3.0 a1-tspl-2009.scm
 # Chapters 6-11
-# guile3.0 -L "${ROOT_DIR}" a2-tspl-2009.scm
+# guile3.0 a2-tspl-2009.scm
 
-# Chapters 1-
-guile3.0 -L "${ROOT_DIR}" b1-grm-2020.scm "${@}"
+guile3.0 b1-grm-2020.scm "${@}"
