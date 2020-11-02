fc4e2252e1322f16481f8d39d31abd00837f06ea
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2020-11-02 10:42:18 +0100

    feat: refine get_estimated_delivery_date

--- postgresql-basics/delivery-date/sql/payment_routing_data.sql
+++ postgresql-basics/delivery-date/sql/payment_routing_data.sql
@@ -4,54 +4,29 @@ SELECT payment_routing.put_pagofx_correspondent (
     a_correspondent_bic := 'BIC-ESBANK',
     a_country := 'ES',
     a_settlement_days := '{1, 2, 3, 4, 5}',
-    a_value_date := 1,
+    a_value_date := 0,
     a_cutoff_time := '17:00:00',
-    -- a_time_zone := '01:00'
     a_time_zone := 'Europe/Madrid'
+    -- a_time_zone := '+01:00'
 ) pagofx_correspondent_id;
 
 SELECT payment_routing.put_pagofx_correspondent (
     a_correspondent_bic := 'BIC-USBANK',
     a_country := 'US',
     a_settlement_days := '{1, 2, 3, 4, 5}',
-    a_value_date := 2,
+    a_value_date := 0,
     a_cutoff_time := '19:00:00',
-    -- a_time_zone := '-1 day, 19:00'
     a_time_zone := 'America/New_York'
+    -- a_time_zone := '-06:00'
 ) pagofx_correspondent_id;
 
 -- Bank holidays
 
-SELECT payment_routing.put_bank_holiday (
-    a_country := 'ES',
-    a_holiday_date := '2020-11-02',
-    -- a_time_zone := '01:00'
-    a_time_zone := 'Europe/Madrid'
-);
-
-SELECT payment_routing.put_bank_holiday (
-    a_country := 'ES',
-    a_holiday_date := '2020-11-10',
-    -- a_time_zone := '01:00'
-    a_time_zone := 'Europe/Madrid'
-);
-
-SELECT payment_routing.put_bank_holiday (
-    a_country := 'US',
-    a_holiday_date := '2020-11-01',
-    -- a_time_zone := '-1 day, 19:00'
-    a_time_zone := 'America/New_York'
-);
+SELECT payment_routing.put_bank_holiday ('ES', '2020-11-02');
+SELECT payment_routing.put_bank_holiday ('US', '2020-11-06');
 
 -- Get estimated delivery date
 
--- SELECT dd.correspondent_bic,
---     dd.country,
---     dd.settlement_days,
---     dd.value_date,
---     dd.cutoff_time,
---     dd.time_zone,
---     dd.delivery_date
--- FROM payment_routing.get_estimated_delivery_date(
---     a_correspondent_bics := '{BIC-ESBANK, BIC-USBANK}'
--- ) dd;
+SELECT *
+FROM payment_routing.get_estimated_delivery_date('{BIC-ESBANK, BIC-USBANK}');
+-- FROM payment_routing.get_estimated_delivery_date('{BIC-ESBANK, BIC-USBANK}', '2020-11-06');
--- postgresql-basics/delivery-date/sql/payment_routing_schema.sql
+++ postgresql-basics/delivery-date/sql/payment_routing_schema.sql
@@ -8,7 +8,10 @@ CREATE TABLE payment_routing.pagofx_correspondent (
     pagofx_correspondent_id uuid NOT NULL
         DEFAULT gen_random_uuid(),
     correspondent_bic varchar(50) NOT NULL,
+    -- PagoFX correspondent location country/area used to lookup bank holidays
     country varchar(50) NOT NULL,
+    -- Days of week on which the PagoFX correspondent accepts payments
+    -- 0 Sunday, 6 Saturday
     settlement_days integer[] NOT NULL
         DEFAULT '{0, 1, 2, 3, 4, 5, 6}',
     -- Time in days taken by the PagoFX correspondent to process the payment
@@ -18,10 +21,8 @@ CREATE TABLE payment_routing.pagofx_correspondent (
     -- calculate the delivery date estimation for the customer
     cutoff_time time NOT NULL
         DEFAULT '24:00:00',
-    -- Time zone of the PagoFX correspondent used to calculate the cutoff_time
+    -- Time zone of the PagoFX correspondent used to compare the cutoff_time
     time_zone varchar(50) NOT NULL,
-    -- Correspondent priority to rank multiple applicable correspondnets.
-    -- Lower value takes priority (ex: 4 is better than 10)
     creation_ts timestamptz NOT NULL
         DEFAULT current_timestamp(3),
     update_ts timestamptz NOT NULL
@@ -31,16 +32,16 @@ CREATE TABLE payment_routing.pagofx_correspondent (
     CONSTRAINT uq_pagofx_correspondent
         UNIQUE (correspondent_bic),
     CONSTRAINT ck_settlement_days_are_valid_week_days
-        CHECK (settlement_days <@ '{0, 1, 2, 3, 4, 5, 6}')
+        CHECK (settlement_days <@ '{0, 1, 2, 3, 4, 5, 6}'),
+    CONSTRAINT ck_value_date_is_non_negative
+        CHECK (value_date >= 0)
 );
 
 CREATE TABLE payment_routing.bank_holiday (
-    -- Country of the bank holiday
+    -- Country/area of the bank holiday
     country varchar(50) NOT NULL,
     -- Bank holiday date
     holiday_date date NOT NULL,
-    -- Bank holiday time zone
-    time_zone varchar(50) NOT NULL,
     creation_ts timestamptz NOT NULL
         DEFAULT current_timestamp(3),
     update_ts timestamptz NOT NULL
@@ -86,91 +87,69 @@ $$;
 
 CREATE OR REPLACE FUNCTION payment_routing.put_bank_holiday (
     a_country varchar(50),
-    a_holiday_date date,
-    a_time_zone varchar(50)
+    a_holiday_date date
 ) RETURNS void
 LANGUAGE sql AS $$
-INSERT INTO payment_routing.bank_holiday (country, holiday_date, time_zone)
-VALUES (a_country, a_holiday_date, a_time_zone)
+INSERT INTO payment_routing.bank_holiday (country, holiday_date)
+VALUES (a_country, a_holiday_date)
 ON CONFLICT ON CONSTRAINT uq_country_holiday_date DO UPDATE SET
-    time_zone = excluded.time_zone,
     update_ts = current_timestamp(3);
 $$;
 
 CREATE OR REPLACE FUNCTION payment_routing.get_estimated_delivery_date(
     a_correspondent_bics varchar(50)[],
-    a_settlement_date timestamptz DEFAULT current_timestamp
+    a_settlement_date date DEFAULT current_date
 ) RETURNS TABLE (
-    pagofx_correspondent_id uuid,
     correspondent_bic varchar(50),
     country varchar(50),
     settlement_days integer[],
     value_date integer,
     cutoff_time time,
-    time_zone varchar(50)
-    -- delivery_date timestamptz
+    time_zone varchar(50),
+    delivery_date date
 ) LANGUAGE sql AS $$
-WITH correspondent (
-    pagofx_correspondent_id,
-    correspondent_bic,
-    country,
-    settlement_days,
-    value_date,
-    cutoff_time,
-    time_zone,
-    settlement_day
-) AS (
-SELECT c.pagofx_correspondent_id,
-    c.correspondent_bic,
-    c.country,
-    c.settlement_days,
-    c.value_date,
-    c.cutoff_time,
-    c.time_zone,
-    d.date
-FROM payment_routing.pagofx_correspondent c,
-    (SELECT current_date + d.n FROM generate_series(0, 14, 1) d(n)) d(date)
-WHERE c.correspondent_bic = ANY ('{BIC-ESBANK}')
-    AND extract(dow FROM d.date) = ANY (c.settlement_days)
-)
-
-SELECT c.*, c.value_date delivery_date
-FROM correspondent c
-    LEFT JOIN payment_routing.bank_holiday bh
-        ON bh.country = c.country AND bh.holiday_date = c.settlement_day
-WHERE bh.holiday_date IS NULL
-;
-$$;
-
-
+-- Calculate the availalbe settlement days for each of the PagoFX correspondents
 WITH available_settlement_day AS (
-SELECT c.correspondent_bic,
-    c.country,
-    c.settlement_days,
-    c.value_date,
-    c.cutoff_time,
-    c.time_zone,
-    d.date settlement_day
+SELECT c.*, d.date settlement_day
 FROM payment_routing.pagofx_correspondent c,
-    (SELECT current_date + d.n FROM generate_series(0, 14, 1) d(n)) d(date)
-WHERE c.correspondent_bic = ANY ('{BIC-ESBANK, BIC-USBANK}')
+    -- All successive dates from the requested settlement date
+    (SELECT a_settlement_date + d.n FROM generate_series(0, 14, 1) d(n)) d(date)
+-- Select only requested PagoFX correspondents
+WHERE c.correspondent_bic = ANY (a_correspondent_bics)
+    -- Select only only days when the PagoFX correspondent is operating
     AND extract(dow FROM d.date) = ANY (c.settlement_days)),
-
+-- Calculate the estimated value date for each of the PagoFX correspondents
+-- taking into consideration bank holidays, time zone, and cutoff time
 effective_value_date AS (
 SELECT sd.*,
+    -- Always add value date of the PagoFX correspondent to the effective value date
     (sd.value_date +
-    CASE
-        WHEN ('2020-11-01 24:00:00 Europe/Madrid'::timestamptz
-            AT TIME ZONE 'America/New_York')::time > sd.cutoff_time THEN 1
+    -- Add 1 more day to the effective value date only if the settlement date is today
+    -- and the settlement time is after the cutoff time in a time zone of the PagoFX
+    -- correspondent
+    CASE WHEN (a_settlement_date = current_date)
+        AND (current_timestamp AT TIME ZONE sd.time_zone)::time > sd.cutoff_time
+        THEN 1
         ELSE 0
-    END
-    ) effective_value_date,
-    rank() OVER (PARTITION BY sd.correspondent_bic ORDER BY sd.settlement_day) rank
+    END) effective_value_date,
+    -- Assign sequential numbers to the settlement days
+    rank() OVER (PARTITION BY sd.correspondent_bic ORDER BY sd.settlement_day)
+    settlement_day_rank
 FROM available_settlement_day sd
+    -- Attach bank holiday dates for a country/area of the PagoFX correspondent
     LEFT JOIN payment_routing.bank_holiday bh
         ON bh.country = sd.country AND bh.holiday_date = sd.settlement_day
+-- Select only working days of the PagoFX correspondent
 WHERE bh.holiday_date IS NULL)
 
-SELECT vd.*, vd.rank = vd.effective_value_date + 1
+SELECT vd.correspondent_bic,
+    vd.country,
+    vd.settlement_days,
+    vd.value_date,
+    vd.cutoff_time,
+    vd.time_zone,
+    vd.settlement_day delivery_date
 FROM effective_value_date vd
-;
+-- Select the first available settlement day that satisfies the effective value date
+WHERE vd.settlement_day_rank = vd.effective_value_date + 1;
+$$;
